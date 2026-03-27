82538a34fff0ff873267dc5151d45c10b5128a6b
jakobmakela <156580100+jakob-ha@users.noreply.github.com>
2026-03-27 17:09:00 +0200

    safeareaview

--- parliament-browser/App.js
+++ parliament-browser/App.js
@@ -1,19 +1,26 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+          <StatusBar style="auto" />
+        </View>
+        <Text>Open up App.js to start working on your app!</Text>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#ffffffff",
     alignItems: "center",
     justifyContent: "center",
   },
